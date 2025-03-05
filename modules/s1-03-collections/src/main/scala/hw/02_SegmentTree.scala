4b6e56d31187bcca5282efcda8cc4852ac777cf5
martynenko arsenii <martynenko.arsenii@phystech.edu>
2025-03-05 21:00:01 +0300

    Complete 3rd hw

--- modules/s1-03-collections/src/main/scala/hw/02_SegmentTree.scala
+++ modules/s1-03-collections/src/main/scala/hw/02_SegmentTree.scala
@@ -64,7 +64,15 @@ sealed trait SegmentTree[A]:
   *          /  \   / \   / \   / \
   *         5    2 4   0 6   7 0   0
   */
-  def update(idx: Int, a: A): SegmentTree[A] = ???
+  def update(idx: Int, a: A): SegmentTree[A] =
+    this match
+      case Leaf(idx, value, _) => Leaf(startIdx, a, monoid)
+      case Node(value, startIdx, endIdx, left, right, monoid) =>
+        val mid = (startIdx + endIdx) / 2
+        val l = if (idx <= mid) left.update(idx, a) else left
+        val r = if (idx > mid) right.update(idx, a) else right
+        val nvalue = monoid.combine(l.value, r.value)
+        Node(nvalue, startIdx, endIdx, l, r, monoid)
 
   /**
   * II. 3.
@@ -80,7 +88,15 @@ sealed trait SegmentTree[A]:
   *  SegmentTree(5, 2, 4, 1, 6, 7)(Monoid.intSum).calc(1, 5) = 20
   *  SegmentTree(5, 2, 4, 1, 6, 7)(Monoid.intSum).calc(1, 4) = 13
   */
-  def calc(from: Int, to: Int): A = ???
+  def calc(from: Int, to: Int): A =
+    this match
+      case Node(value, startIdx, endIdx, left, right, _) if from <= startIdx && to >= endIdx => value
+      case Node(_, startIdx, endIdx, left, right, _) =>
+        val lValue = if (from <= left.endIdx) left.calc(from, to) else monoid.empty
+        val rValue = if (to >= right.startIdx) right.calc(from, to) else monoid.empty
+        monoid.combine(lValue, rValue)
+      case Leaf(valueIdx, value, _) if valueIdx >= from && valueIdx <= to => value
+      case _                                                              => monoid.empty
 
 case class Leaf[A](idx: Int, value: A, monoid: Monoid[A]) extends SegmentTree[A]:
   val startIdx = idx
@@ -114,7 +130,21 @@ object SegmentTree:
    *          /  \   / \   / \   / \
    *         5    2 4   1 6   7 0   0
    */
-  def apply[A](values: A*)(monoid: Monoid[A]): SegmentTree[A] = ???
+  def apply[A](values: A*)(monoid: Monoid[A]): SegmentTree[A] =
+    val newSize = math.pow(2, math.ceil(math.log(values.length) / math.log(2))).toInt
+    val newSeq = values.toSeq ++ Seq.fill(newSize - values.length)(monoid.empty)
+    buildTree(newSeq.toIndexedSeq, 0, newSize - 1, monoid)
+
+  def buildTree[A](values: IndexedSeq[A], startIdx: Int, endIdx: Int, monoid: Monoid[A]): SegmentTree[A] =
+    if (startIdx == endIdx) {
+      Leaf(startIdx, values(startIdx), monoid)
+    } else {
+      val mid = (startIdx + endIdx) / 2
+      val l = buildTree(values, startIdx, mid, monoid)
+      val r = buildTree(values, mid + 1, endIdx, monoid)
+      val value = monoid.combine(l.value, r.value)
+      Node(value, startIdx, endIdx, l, r, monoid)
+    }
 
   /**
     * II. 4. Вопрос: как будет выглядеть код, если вам потребуется считать результаты нескольких операций
@@ -122,3 +152,13 @@ object SegmentTree:
     *
     *    Потребуется ли делать дополнительную структуру данных? Или расширять текущую?
     */
+
+  // В целом, у нас в структуре вписана используемая операция, т.е. если дерево построено для суммы,
+  // то мы в не-листьях храним сумму элементов-детей. Если мы знаем, какие операции нам надо поддерживать,
+  // то можно строить дерево сразу для нескольких операций (и обновлять для нескольких), храня в родителях
+  // что-то вроде мапы или массива, где мы знаем какой индекс за какую операцию отвечает.
+  // Правда эта реализация, по сути, почти ничем не будет отличаться (в смысле времени и памяти)
+  // от просто нескольких деревьев, а также будет работать только если нейтральный элемент наших ассоциативных
+  // операций совпадает. Иначе же для построения дерева мы будем давать на вход массивы с разными
+  // хвостами (условно нули на конце для суммы и единички для умножения), тогда уже придется разбить на несколько
+  // структур. Но с нашей хорошей "шаблонной" реализацией ничего нового дописывать не придется.
