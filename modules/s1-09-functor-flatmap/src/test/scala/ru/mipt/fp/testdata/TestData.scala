375d1d8a2b690bcc4be8f035115f51353dababc6
martynenko arsenii <martynenko.arsenii@phystech.edu>
2025-04-27 00:01:02 +0300

    Fix warns problem

--- modules/s1-09-functor-flatmap/src/test/scala/ru/mipt/fp/testdata/TestData.scala
+++ modules/s1-09-functor-flatmap/src/test/scala/ru/mipt/fp/testdata/TestData.scala
@@ -6,21 +6,21 @@ import scala.concurrent.duration._
 
 object TestData {
 
-  val ttl      = 1.second
+  val ttl = 1.second
   val clientId = ClientId("Client")
 
-  val ucid           = Ucid("Ucid")
-  val cardNumber     = CardNumber("1234-5678-9012-3456")
-  val cvv            = CardCvv("123")
+  val ucid = Ucid("Ucid")
+  val cardNumber = CardNumber("1234-5678-9012-3456")
+  val cvv = CardCvv("123")
   val expirationDate = CardExpirationDate("01/20")
-  val card           = Card(ucid, cardNumber, cvv, expirationDate)
+  val card = Card(ucid, cardNumber, cvv, expirationDate)
 
   val cards = List(card)
 
-  val maskedCardNumber     = CardNumber("1234-****-****-*456")
-  val maskedCvv            = CardCvv("***")
+  val maskedCardNumber = CardNumber("1234-****-****-*456")
+  val maskedCvv = CardCvv("***")
   val maskedExpirationDate = CardExpirationDate("**\\**")
-  val maskedCard           = Card(ucid, maskedCardNumber, maskedCvv, maskedExpirationDate)
+  val maskedCard = Card(ucid, maskedCardNumber, maskedCvv, maskedExpirationDate)
 
   val maskedCards = List(maskedCard)
 
