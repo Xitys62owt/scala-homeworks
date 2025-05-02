2fa5a49c4f96d79a301af5c7008c326ce4f52dba
martynenko arsenii <martynenko.arsenii@phystech.edu>
2025-05-03 01:27:52 +0300

    Complete 10hw

--- modules/s1-10-monad-errors/src/main/scala/ru/mipt/fp/resilience/FallbackCache.scala
+++ modules/s1-10-monad-errors/src/main/scala/ru/mipt/fp/resilience/FallbackCache.scala
@@ -5,6 +5,8 @@ import ru.mipt.fp.utils.Cache
 
 import scala.annotation.nowarn
 
+import cats.syntax.all._
+
 /**
  * Интерфейс для фоллбэк-кэша
  */
@@ -15,7 +17,17 @@ object FallbackCache:
   @nowarn
   private class Impl[F[_], K, V, E](cache: Cache[F, K, V])(using monadError: MonadError[F, E])
     extends FallbackCache[F, K, V]:
-    def withFallback(key: K)(operation: K => F[V]): F[V] = ???
+    def withFallback(key: K)(operation: K => F[V]): F[V] =
+      val result = for {
+        value <- operation(key)
+        _ <- cache.put(key, value)
+      } yield value
+      result.handleErrorWith { error =>
+        cache.get(key).flatMap {
+          case Some(value) => MonadError[F, E].pure(value)
+          case None        => MonadError[F, E].raiseError(error)
+        }
+      }
 
   def apply[F[_], K, V, E](cache: Cache[F, K, V])(using monadError: MonadError[F, E]): FallbackCache[F, K, V] =
     new Impl(cache)
